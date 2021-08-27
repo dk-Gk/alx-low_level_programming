ad9020c6d4ecd13585ce2df0e006546b612885b2
dk-Gk <idkin19@gmail.com>
2021-08-27 20:35:14 +0000

    print whether the number stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-if (n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is positive\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
