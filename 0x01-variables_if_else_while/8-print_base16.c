b0b494c8291554deb702cef4388e728afd4bf1e7
dk-Gk <idkin19@gmail.com>
2021-08-27 23:41:07 +0000

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints a base 16
+ * Return: returns 0
+ */
+int main(void)
+{
+char ch = 0;
+char cch = 'a';
+for (ch = 0; ch <= 10; ch++)
+{
+if (ch = 10)
+{
+for (cch = 'a'; cch <=f; cch++)
+{
+putchar(cch);
+}
+}
+else
+{
+putchar(ch)
+}
+}
+putchar('\n');
+return (0);
+}
