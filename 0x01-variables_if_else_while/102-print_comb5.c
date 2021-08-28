d3700a460427e0371c303c59238f38c41c5f2de1
dk-Gk <idkin19@gmail.com>
2021-08-28 02:09:43 +0000

    a program that prints all possible different combinations of two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,17 @@ int main(void)
 {
 int i;
 int j;
+int z;
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
-putchar((i % 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
+for (z = j+1;  < 100; j++)
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
+putchar(' ');
+putchar((i % 10) + '0');
+putchar((z % 10) + '0');
 if (i < 9)
 {
 putchar(44);
