4fb5dd404d1ff6e4afb190e150b6065fc08d7360
dk-Gk <idkin19@gmail.com>
2021-08-28 01:00:45 +0000

    a program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@ int main(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-putchar((i%10) + '0');
+putchar((i % 10) + '0');
 if (i < 9)
 {
 putchar(44);
 putchar(' ');
 }
-putchar('\n');
 }
+putchar('\n');
 return (0);
 }
