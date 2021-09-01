ddf30fc7fc531aedfa71399b0af87ddbe7acf69c
dk-Gk <idkin19@gmail.com>
2021-09-01 16:37:25 +0000

    a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - check alphabets
+ *@c: the number
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+char b;
+char d;
+int i = 0;
+for (d = 'A'; d <= 'Z'; d++)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+if (c == b || c == d)
+{
+i = 1;
+}
+}
+}
+return (i);
+}
