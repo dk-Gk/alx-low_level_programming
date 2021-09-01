c59ca9d0e65b560e457abb36924f9f5b05136e77
dk-Gk <idkin19@gmail.com>
2021-09-01 17:11:02 +0000

    a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes alphabet
+ *@n: absolute
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _abs(int);
 int print_sign(int n);
 int _putchar(char c);
 void print_alphabet_x10(void);
