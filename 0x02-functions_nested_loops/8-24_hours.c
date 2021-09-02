a2d1a99309aa0ee0ff39353800ce5a93e0f1b5f3
dk-Gk <idkin19@gmail.com>
2021-09-02 02:36:59 +0000

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,12 @@ for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
-if (i < j && i != j)
-{
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
 _putchar(58);
-_putchar ((j / 10) + '0');
-_putchar ((j % 10) + '0');
-}
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
 }
 }
 }
