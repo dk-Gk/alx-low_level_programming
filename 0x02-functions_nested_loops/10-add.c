1d4c15c369dae338c30fd4cc19352c250c42fc58
dk-Gk <idkin19@gmail.com>
2021-09-03 20:52:21 +0000

    a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,21 +1,13 @@
 #include "main.h"
 /**
- * jack_bauer - computes alphabet
- * Return: Always 0
+ * add - adds 2 num
+ * @i: first num
+ * @l: Second num
+ * Return: return sum
  */
-void jack_bauer(void)
+int add(int i, int j)
 {
-	int i, j;
-	for (i = 0; i < 24; i++)
-	{
-		for (j = 0; j < 60; j++)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(58);
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-		}
-	}
+int s;
+s = i + j;
+return (s);
 }
