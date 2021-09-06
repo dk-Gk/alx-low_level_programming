ec4f7330d2c2b2030ee50948cc86739411778ac6
dk-Gk <idkin19@gmail.com>
2021-09-06 15:40:00 +0000

    a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: pointer value
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+int l = 0;
+while (*s != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
