bae3f156d266227598f54ea9c31ec4051723edc5
dk-Gk <idkin19@gmail.com>
2021-09-07 04:18:23 +0000

    a program that generates random valid passwords for the program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,18 +10,20 @@
 int main(void)
 {
 int i = 0;
-int N = 10;
+int n = 0;
 int randomizer = 0;
 srand((unsigned int)(time(NULL)));
-char numbers[] = "0123456789";
-char letter[] = "abcdefghijklmnoqprstuvwyzx";
-char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-char symbols[] = "!@#$^&*?";
-char password[10];
+char numbers [] = "1234567890";
+char letter [] = "abcdefghijklmnoqprstuvwyzx";
+char letterr [] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+char symbols [] = "!@#$%^&*(){}[]:<>?,./";
+printf("\nHow long password:");
+scanf("%d", &n);
+char password[n];
 randomizer = rand() % 4;
-for (i = 0; i < N; i++)
+for (i=0;i<n;i++)
 {
-if (randomizer == 1)
+if(randomizer == 1)
 {
 password[i] = numbers[rand() % 10];
 randomizer = rand() % 4;
@@ -29,22 +31,22 @@ printf("%c", password[i]);
 }
 else if (randomizer == 2)
 {
-password[i] = symbols[rand() % 8];
+password[i] = symbols[rand() % 26];
 randomizer = rand() % 4;
 printf("%c", password[i]);
 }
 else if (randomizer == 3)
 {
-password[i] = LETTER[rand() % 26];
+password[i] = letterr[rand() % 26];
 randomizer = rand() % 4;
 printf("%c", password[i]);
 }
 else
 {
-password[i] = letter[rand() % 26];
+password[i] = letter[rand() % 21];
 randomizer = rand() % 4;
 printf("%c", password[i]);
 }
 }
-return (0);
+return main();
 }
