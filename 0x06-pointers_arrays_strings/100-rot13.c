3b50f0ddb23dc41a7e69511cb6d5d6f96c19f4ce
dk-Gk <idkin19@gmail.com>
2021-09-10 04:09:35 +0000

    a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,18 +9,16 @@ char *rot13(char *rot)
 int i, j;
 char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char e[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-for (i = 0; rot[i] != '\0';)
+for (i = 0; rot[i] != '\0'; i++)
 {
-i++;
-}
-for (j = 0; d[j] != '\0';)
+for (j = 0; d[j] != '\0'; j++)
 {
-j++;
-}
 if (rot[i] == d[j])
 {
 rot[i] = e[j];
 break;
+}
+}
 }
 return (rot);
 }
