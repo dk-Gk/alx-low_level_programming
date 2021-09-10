37a9f74ebfdc85732b37fdfffb117f1b2bf5d458
dk-Gk <idkin19@gmail.com>
2021-09-10 03:40:07 +0000

    a function that encodes a string into 1337 or leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @c: pointer
+ * Return: c
+ */
+char *leet(char *c)
+{
+int i,j;
+char l[] = "aeotl";
+char ll[] = "AEOTL";
+char n[] = "43071";
+for (i = 0; c[i] != '\0'; i++)
+{
+for (j = 0; l[j] != '\0'; j++)
+{
+if (c[i] == l[j] || c[i] == ll[j])
+c[i] = numbers[j];
+}
+}
+return (c);
+}
