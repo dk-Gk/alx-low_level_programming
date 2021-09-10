d2f41f958d0ed3adcf9fa2c6bcd4777f1ea6681d
dk-Gk <idkin19@gmail.com>
2021-09-10 02:36:23 +0000

    a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncat - a function that concatinate strings
+ *@dest: pointer of string
+ *@src: pointer of sring
+ *@n: size
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0, k = 0;
+for (; dest[k];)
+{
+k++;
+}
+while (j < n && src[j])
+{
+dest[k + i] = src[j];
+i++;
+j++;
+}
+return (dest);
+}
