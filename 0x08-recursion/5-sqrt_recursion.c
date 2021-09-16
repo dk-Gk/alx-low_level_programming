f6e262c4e768ee396892460f0e2bcc93f9e21c73
dk-Gk <idkin19@gmail.com>
2021-09-16 02:28:54 +0000

    a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * pow - calculate the pow
+ *@i: number
+ *@n: number
+ * Return: result
+ */
+int pow(int i, int n)
+{
+if ((i * i) > n)
+return (-1);
+if (i * i == n)
+return (i);
+i++;
+return (pow(i, n));
+}
+/**
  * _sqrt_recursion - calculate square root of an integer
  * @n: number
  * Return: result
