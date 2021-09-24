47c9b7cd964f77d3ae3fc4707cbfe2b33895fb90
dk-Gk <idkin19@gmail.com>
2021-09-24 03:29:08 +0000

    a program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints buffer in hexa
+ * @argc: the address of memory to print
+ * @argv: the size of the memory to print
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int size1 = 0, size2 = 0, i = 0, t = 0, p = 0, o = 0, ps = 0;
+while (argv[1][size1])
+{
+size1++;
+}
+while (argv[2][size2])
+{
+size2++;
+}
+ps = size1 + size2 + 1;
+p = malloc(sizeof(char) * ps);
+if (!p)
+return (98);
+while (ps >= 0)
+p[ps] = '0';
+size1--;
+size2--;
+while (size2 >= 0)
+{
+i = t;
+for (j = size1; j >= 0; j--)
+{
+p = (argv[1][j]-'0') * (argv[2][size2]-'0');
+p += p[index] -'0';
+p += o;
+o = p / 10;
+p[i] = p % 10 + '0';
+i++;
+}
+if (o)
+{
+p[index] = o;
+i++;
+}
+o = 0;
+size2--;
+t++;
+}
+p[i] ='\0';
+printf("%s\n", p);
+free(p);
+return (0);
+}
