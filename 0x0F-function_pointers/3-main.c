bd630b10f7b1060c56183c635909c44904b2617e
dk-Gk <idkin19@gmail.com>
2021-09-30 22:44:38 +0000

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 int (*r)(int, int), a, b;
 a = atoi(argv[1]);
 b = atoi(argv[3]);
-if(argc != 4)
+if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 if ((*argv[2] != '+' &&	*argv[2] != '-' && *argv[2] != '/' && *argv[2] != '*'
-     &&*argv[2] != '%')
-    || argv[2][1] != '\0')
+&& *argv[2] != '%')
+|| argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
