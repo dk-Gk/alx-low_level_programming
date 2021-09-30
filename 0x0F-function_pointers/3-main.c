575537ce6527717b6e3801495e903ce71e1a700a
dk-Gk <idkin19@gmail.com>
2021-09-30 22:41:01 +0000

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,27 +12,26 @@
 int main(int argc, char *argv[])
 {
 int (*r)(int, int), a, b;
-char *j;
-j = argv[2];
 a = atoi(argv[1]);
 b = atoi(argv[3]);
 if(argc != 4)
 {
-printf("Error /n");
+printf("Error\n");
 exit(98);
 }
-printf("%s /n", j);
-if (*j != '+' && *j != '-' && *j != '/' && *j != '*' && *j != '%')
+if ((*argv[2] != '+' &&	*argv[2] != '-' && *argv[2] != '/' && *argv[2] != '*'
+     &&*argv[2] != '%')
+    || argv[2][1] != '\0')
 {
-printf("Error /n");
+printf("Error\n");
 exit(99);
 }
-if ((*argv[2] == '/' || *argv[2] == '%') || b == 0)
+if ((*argv[2] == '/' || *argv[2] == '%') && (b == 0))
 {
-printf("Error /n");
+printf("Error\n");
 exit(100);
 }
 r = get_op_func(argv[2]);
-printf("%d /n", r(a, b));
+printf("%d\n", r(a, b));
 return (0);
 }
