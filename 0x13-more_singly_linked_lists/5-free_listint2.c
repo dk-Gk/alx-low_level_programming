8db0f20da2128d153b0aac1511096313186c36c9
dk-Gk <idkin19@gmail.com>
2021-10-12 21:10:11 +0000

    a function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list_t list
+ * @head: head
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+list_t *fr;
+while (*head)
+{
+fr = *head;
+*head = *head->next;
+free(fr->n);
+free(p);
+}
+*head = NULL;
+}
