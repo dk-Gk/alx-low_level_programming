da265fe93f6d4253e396296475d2c2bde84b07a4
dk-Gk <idkin19@gmail.com>
2021-10-19 15:25:58 +0000

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,17 +27,11 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t w;
-/*size_t count = 0;
-while ( *text_content != '\0')
-{
-count++;
-text_content++;
-}*/
 if (filename == NULL)
 {
 return (-1);
 }
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 {
 return (-1);
