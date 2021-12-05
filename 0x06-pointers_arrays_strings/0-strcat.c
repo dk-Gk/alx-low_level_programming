0adf8ac8075c6b1495c7d85aa9fb1cec0094c306
dk-Gk <idkin19@gmail.com>
2021-12-05 01:00:57 +0000

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,8 +7,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-for (i = 0; dest[i] != '\0'; ++i)
+int i = 0, j;
+while (dest[i] != '\0')
+{
+i++;
+}
 for (j = 0; src[j] != '\0'; ++j, ++i)
 {
 dest[i] = src[j];
